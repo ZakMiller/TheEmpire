c2010bb5e85b1183335fd18ad106d238352a568a
nmatkins <nmatkins@gmail.com>
2017-01-28 14:43:06 -0500

    the stuff

---
+++ game1/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//return true or false if message meets criteria
+function isMessageValid(){
+
+}
+
+//sends message to the server/terminal
+function sendMessage(){
+
+
+}
---
+++ game1/server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/",)
