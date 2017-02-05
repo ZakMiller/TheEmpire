5787beb86179fbec5d5757f0ff125a21ed00c438
zak <zak@zakmiller.com>
2017-02-05 14:41:29 -0500

    adding linter, changing files to conform

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,42 +1,37 @@
-'use strict';
+'use strict'
 
-const socket = require('socket.io-client')();
-var username = prompt('enter your name');
+const socket = require('socket.io-client')()
+var username = prompt('enter your name')
 
-socket.on('incomingMessage', appendIncomingMessage);
+socket.on('incomingMessage', appendIncomingMessage)
 
-//return true or false if message meets criteria
-function isMessageValid() {
-
-}
-
-document.querySelector('#input').addEventListener("input", function(event) {
-  //need to create expression that alerts if invalid character is attempted
-  this.value = this.value.toUpperCase();
-  this.value = this.value.replace(/[^A-Z ]/g, "");
-});
+document.querySelector('#input').addEventListener('input', function(event) {
+  // need to create expression that alerts if invalid character is attempted
+  this.value = this.value.toUpperCase()
+  this.value = this.value.replace(/[^A-Z ]/g, '')
+})
 
 function appendIncomingMessage(newMessage) {
-  var item = document.createElement("li");
-  item.appendChild(document.createTextNode(`<${newMessage.name}> ${newMessage.message}`));
-  document.getElementById('messages').appendChild(item);
+  var item = document.createElement('li')
+  item.appendChild(document.createTextNode(`<${newMessage.name}> ${newMessage.message}`))
+  document.getElementById('messages').appendChild(item)
 }
 
-//sends message to the server/terminal
+// sends message to the server/terminal
 function sendMessage() {
-  var inputText = document.getElementById('input').value;
-  document.getElementById('input').value = '';
+  var inputText = document.getElementById('input').value
+  document.getElementById('input').value = ''
 
   socket.emit('message', {
     name: username,
     message: inputText
-  });
+  })
 }
 
-document.getElementById("input").addEventListener("keyup", function(event) {
-  event.preventDefault();
-  var ENTER_KEY = 13;
+document.getElementById('input').addEventListener('keyup', function(event) {
+  event.preventDefault()
+  var ENTER_KEY = 13
   if (event.keyCode === ENTER_KEY) {
-    sendMessage();
+    sendMessage()
   }
-});
+})
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
-const express = require("express");
-const path = require("path");
-const app = express();
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
+const express = require('express')
+const path = require('path')
+const app = express()
+const httpServer = require('http').createServer(app)
+const io = require('socket.io')(httpServer)
 
-httpServer.listen(80);
+httpServer.listen(80)
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-io.on('connection', handleSocketConnection);
+io.on('connection', handleSocketConnection)
 
-function handleSocketConnection(socket) {
-  socket.on('message', data => {
+function handleSocketConnection (socket) {
+  socket.on('message', (data) => {
     io.emit('incomingMessage', {
       name: data.name,
       message: data.message
-    });
-  });
+    })
+  })
 }
