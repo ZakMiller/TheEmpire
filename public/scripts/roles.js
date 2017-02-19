e9f6090ae0e5b7d845fd41fa0670216c13a18a89
zak <zak@zakmiller.com>
2017-02-19 14:56:14 -0500

    Role Distribution

--- public/scripts/roles.js
+++ public/scripts/roles.js
@@ -1,25 +1,19 @@
-class Role {
-  constructor(image, name, description) {
-    this.image = image
-    this.name = name
-    this.description = description
+const roles = {
+  human: {
+    image: 'https://s-media-cache-ak0.pinimg.com/originals/e0/f8/41/e0f84168cde4123e1e5aec348364a017.jpg',
+    name: 'Human',
+    description: "You can say whatever you want, but be careful! If the robots figure out you're human it 's all over!"
+  },
+  AI: {
+    image: 'http://www.popsci.com/sites/popsci.com/files/styles/large_1x_/public/images/2014/11/robot-friend-popular-science.jpg?itok=bX08qSI7',
+    name: 'AI',
+    description: "Your programming dictates that you have to use three out of the ten 'special' words, but be careful! If the judge thinks you're the worst AI it's all over!"
   }
 }
-module.exports = {
-  getHumanRole() {
-    const image = 'https://s-media-cache-ak0.pinimg.com/originals/e0/f8/41/e0f84168cde4123e1e5aec348364a017.jpg'
-    const name = 'Human'
-    const description = "You can say whatever you want, but be careful! If the robots figure out you're human it's all over!"
-    const human = new Role(image, name, description)
-    return human
-  },
 
-  getAIRole() {
-    const image = 'http://www.popsci.com/sites/popsci.com/files/styles/large_1x_/public/images/2014/11/robot-friend-popular-science.jpg?itok=bX08qSI7'
-    const name = 'AI'
-    const description = "Your programming dictates that you have to use three out of the ten 'special' words,\
-    but be careful! If the judge thinks you're the worst AI it's all over!"
-    const AI = new Role(image, name, description)
-    return AI
+module.exports = {
+  getRole(name) {
+    return roles[name]
   }
+
 }
