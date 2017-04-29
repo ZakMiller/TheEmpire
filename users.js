b621417cd3c4a07c16a4ec4e0d6130df551803c2
SylvanSign <jake.d.bray@gmail.com>
2017-04-29 15:56:20 -0400

    add voting system and endgame (#50)

--- users.js
+++ users.js
@@ -1,63 +1,72 @@
 'use strict'
 
-// const names = new Set()
-let names = {}
-const votes = {}
+let users = {}
 
 module.exports = {
   add(name, id) {
-    names[name] = {
-      id: id
+    users[name] = {
+      id: id,
+      votes: 0
     }
-    // votes = 0
   },
 
   delete(name) {
-    delete names[name]
+    delete users[name]
   },
 
   has(name) {
-    return names.hasOwnProperty(name)
+    return users.hasOwnProperty(name)
     // do stuff
   },
 
+  getId(name) {
+    if (this.has(name)) {
+      return users[name].id
+    } else {
+      return null
+    }
+  },
+
   list() {
-    return names
+    return users
   },
 
   names() {
-    return Object.keys(names)
+    return Object.keys(users)
   },
 
   count() {
-    return Object.keys(names).length
+    return Object.keys(users).length
   },
 
   clear() {
-    names = {}
+    users = {}
   },
 
   getNameFromID(id) {
-    for (var name in names) {
-      if (names[name].id === id) {
+    for (var name in users) {
+      if (users[name].id === id) {
         return name
       }
     }
   },
 
   vote(name) {
-    // votes[name] += 1
+    if (this.has(name)) {
+      users[name].votes += 1
+    }
   },
 
   getMostVoted() {
-    // let mostVotes = 0
-    // let mostVoted
-    // for (let i = 0; i < names.length; i++) {
-    // if (names[i].votes > mostVotes) {
-    //  mostVotes = votes[i]
-    //  mostVoted = names[i]
-    //   }
-    //  }
-    // / return mostVoted
+    let mostVotes = 0
+    let mostVoted
+    for (const name in users) {
+      const user = users[name]
+      if (user.votes > mostVotes) {
+        mostVotes = user.votes
+        mostVoted = name
+      }
+    }
+    return mostVoted
   }
 }
